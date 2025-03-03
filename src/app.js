4932c2e3d0872ca9d78968ed4c983afca952b5ac
Sierrena Taylor-Seals <sierrena.taylor@outlook.com>
2025-03-03 03:16:57 +0000

    domaingen

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  
+  let article =['the','a'];
+  let adj =['beautiful', 'soft', 'colorful', 'bright'];
+  let noun =['clover', 'pie', 'bowl', 'sword'];
+  let ext =['.com', '.us', '.net', '.io'];
+
+  for(let i=0; i<article.length; i++) {
+    for(let o=0; o<adj.length; o++) {
+      for(let u=0; u<noun.length; u++) {
+        for(let a=0; a<ext.length; a++) {
+        console.log(article[i] + adj[o] + noun[u] + ext[a]);
+
+        }
+      }
+    }
+  }
   console.log("Hello Rigo from the console!");
 };
